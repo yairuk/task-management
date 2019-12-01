209b7a86e7efc9e5012299b5546f9f17bc069272
Uksusman, Yair <yairu@realcommerce.co.il>
2019-12-01 23:08:07 +0200

    onpush

--- src/app/task/sub-task/sub-task.component.ts
+++ src/app/task/sub-task/sub-task.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { ITask, STATUSES_ICONS } from '../task.model';
 
 @Component({
   selector: 'sub-task',
   templateUrl: './sub-task.component.html',
-  styleUrls: ['./sub-task.component.scss']
+  styleUrls: ['./sub-task.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SubTaskComponent {
 
